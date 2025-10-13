2b9be0337cc88b17a3e0ba0af4a30bfdd273e4d5
eselse <ilikewebdevelopment@gmail.com>
2025-10-13 22:14:48 +0400

    add bin and binlist

--- main.go
+++ main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Bin struct {
+	id        string
+	isPrivate bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	bins []Bin
+}
+
+func generateID() string {
+	id := uuid.New()
+	return id.String()
+}
+
+func newBin(name string, isPrivate bool) (*Bin, error) {
+	if name == "" {
+		return &Bin{}, errors.New("name can't be empty")
+	}
+	id := generateID()
+	createdAt := time.Now()
+	result := Bin{
+		id:        id,
+		isPrivate: isPrivate,
+		createdAt: createdAt,
+		name:      name,
+	}
+	return &result, nil
+}
+
 func main() {
+	bin, err := newBin("example", false)
+	fmt.Println(bin, err)
 }
