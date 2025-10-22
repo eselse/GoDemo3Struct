995e3cad4820bb6cc1a2182d1623171a1929ef56
eselse <ilikewebdevelopment@gmail.com>
2025-10-22 14:40:36 +0400

    create save and load from file functions

--- file/file.go
+++ file/file.go
@@ -1,1 +1,28 @@
 package file
+
+import (
+	"fmt"
+	"os"
+)
+
+func ReadFile(fileName string) ([]byte, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func WriteFile(content []byte, name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+	}
+	defer file.Close()
+	_, err = file.Write(content)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("\nFile written successfully")
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,24 @@
 package storage
+
+import (
+	"fmt"
+	"os"
+)
+
+func saveToFile(data []byte, name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+	}
+	defer file.Close()
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("File written successfully")
+}
+
+func GetFromFile() {
+
+}
